12ae08ab62ab958de2a6a34a4199682b2ed4a602
virtual1a <1926616949@qq.com>
2023-09-18 21:03:36 +0800

    fix

--- kernel/trace.c
+++ kernel/trace.c
@@ -1,57 +1,57 @@
-#include "types.h"
-#include "param.h"
-#include "memlayout.h"
-#include "riscv.h"
-#include "spinlock.h"
-#include "proc.h"
-#include "syscall.h"
-#include "defs.h"
-
-
-static char*s[] = {
-"",
-"fork",
-"exit",
-"wait",
-"pipe",
-"read",
-"kill",
-"exec",
-"fstat",
-"chdir",
-"dup",
-"getpid",
-"sbrk",
-"sleep",
-"uptime",
-"open",
-"write",
-"mknod",
-"unlink",
-"link",
-"mkdir",
-"close"
-};
-
-void
-syscall(void)
-{
-  int num;
-  struct proc *p = myproc();
-
-  num = p->trapframe->a7;
-  if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    // Use num to lookup the system call function for num, call it,
-    // and store its return value in p->trapframe->a0
-    p->trapframe->a0 = syscalls[num]();
-  } else {
-    printf("%d %s: unknown sys call %d\n",
-            p->pid, p->name, num);
-    p->trapframe->a0 = -1;
-  }
-  if (log_switch) {
-    printf("syscall %s -> %d\n",
-      s[num], p->pid);
-  }
-
-}
+//#include "types.h"
+//#include "param.h"
+//#include "memlayout.h"
+//#include "riscv.h"
+//#include "spinlock.h"
+//#include "proc.h"
+////#include "syscall.h"
+////#include "defs.h"
+//
+//
+//static char*s[] = {
+//"",
+//"fork",
+//"exit",
+//"wait",
+//"pipe",
+//"read",
+//"kill",
+//"exec",
+//"fstat",
+//"chdir",
+//"dup",
+//"getpid",
+//"sbrk",
+//"sleep",
+//"uptime",
+//"open",
+//"write",
+//"mknod",
+//"unlink",
+//"link",
+//"mkdir",
+//"close"
+//};
+//
+//void
+//syscall(void)
+//{
+//  int num;
+//  struct proc *p = myproc();
+//
+//  num = p->trapframe->a7;
+//  if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
+//    // Use num to lookup the system call function for num, call it,
+//    // and store its return value in p->trapframe->a0
+//    p->trapframe->a0 = syscalls[num]();
+//  } else {
+//    printf("%d %s: unknown sys call %d\n",
+//            p->pid, p->name, num);
+//    p->trapframe->a0 = -1;
+//  }
+//  if (log_switch) {
+//    printf("syscall %s -> %d\n",
+//      s[num], p->pid);
+//  }
+//
+//}
